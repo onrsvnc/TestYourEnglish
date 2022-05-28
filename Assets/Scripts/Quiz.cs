c8dc9ef07a326c03dc430090942fc842e053b60b
onrsvnc <onrsvnc@hotmail.com>
2022-05-28 17:16:46 +0300

    ScoreKeeper script has been added, Quiz Script has been modified by Line 24 and below to work with it, Score Keeping Text added to unity

--- Assets/Scripts/Quiz.cs
+++ Assets/Scripts/Quiz.cs
@@ -20,12 +20,15 @@ public class Quiz : MonoBehaviour
     [Header("Timer")]
     [SerializeField] Image timerImage;
     Timer timer;
+    [Header("Scoring")]
+    [SerializeField] TextMeshProUGUI scoreText;
+    ScoreKeeper scoreKeeper;
 
 
-   
     void Start()
     {
         timer = FindObjectOfType<Timer>();
+        scoreKeeper = FindObjectOfType<ScoreKeeper>();
     }
 
     void Update() 
@@ -51,6 +54,7 @@ public class Quiz : MonoBehaviour
         DisplayAnswer(index);
         SetButtonState(false);
         timer.CancelTimer();
+        scoreText.text = "Score: " + scoreKeeper.CalculateScore() + "%";
     }
 
     void DisplayAnswer(int index)
@@ -62,6 +66,7 @@ public class Quiz : MonoBehaviour
             questionText.text = "Correct!";
             buttonImage = answerButtons[index].GetComponent<Image>();
             buttonImage.sprite = correctAnswerSprite; 
+            scoreKeeper.IncrementCorrectAnswers();
         }
         else
         {
@@ -81,6 +86,7 @@ public class Quiz : MonoBehaviour
             SetDefaultButtonSprites();
             GetRandomQuestion();
             DisplayQuestion();
+            scoreKeeper.IncrementQuestionsSeen();
         }
 
     }
